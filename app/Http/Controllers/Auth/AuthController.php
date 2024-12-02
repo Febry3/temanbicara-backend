811e6c596c7450f246373d775829a4a047561ac5
Fehehehe <febryt22@gmail.com>
2024-12-02 21:40:15 +0700

    initial commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public static function login(Request $request) {}
+
+    public static function register(Request $request) {}
+
+    public static function logout(Request $request) {}
+}

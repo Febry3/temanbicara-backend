9c8c5f3d6a6b4012e0f3f96ac4fed5ca11a0158a
Fehehehe <febryt22@gmail.com>
2024-12-02 22:02:23 +0700

    feat: finisihing login v1

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,13 +3,72 @@
 namespace App\Http\Controllers\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public static function login(Request $request) {}
+    public static function register(Request $request)
+    {
+        try {
+            $requestedData = $request->only([
+                'first_name',
+                'last_name',
+                'email',
+                'phone_number',
+                'password'
+            ]);
 
-    public static function register(Request $request) {}
+            $validateData = Validator::make([
+                $requestedData,
+                [
+                    'first_name' => 'required',
+                    'last_name' => 'required',
+                    'email' => 'required|unique:users,email|email',
+                    'phone_number' => 'required|unique:users,phone_number',
+                    'password' => 'required',
+                ]
+            ]);
+
+            if ($validateData->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Terjadi kesalahan pada validasi',
+                    'error' => $validateData->errors(),
+                ], 200);
+            };
+
+            $user = User::create([
+                'first_name' => $requestedData['first_name'],
+                'last_name' => $requestedData['last_name'],
+                'email' => $requestedData['email'],
+                'phone_number' => $requestedData['phone_number'],
+                'password' => $requestedData['password'],
+            ]);
+
+            return response()->json(
+                [
+                    'status' => true,
+                    'message' => 'Akun berhasil dibuat',
+                    'token' => $user->createToken('RevanGay')->plainTextToken,
+                    'data' => $user,
+                ],
+                200
+            );
+        } catch (\Throwable $err) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => $err->getMessage()
+                ],
+                500
+            );
+        }
+    }
+
+    public static function login(Request $request) {}
 
     public static function logout(Request $request) {}
 }

6c1369f18f32d1385bb58fa8ba81a85019a39039
Fehehehe <febryt22@gmail.com>
2024-12-03 11:22:29 +0700

    feat: adding forget and change password api

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -148,13 +148,15 @@ class AuthController extends Controller
             $requestedData = $request->only([
                 'old_password',
                 'new_password',
+                'confirm_password',
             ]);
 
             $validateData = Validator::make(
                 $requestedData,
                 [
                     'old_password' => 'required',
-                    'new_password' => 'required'
+                    'new_password' => 'required',
+                    'confirm_password' => 'required',
                 ]
             );
 
@@ -165,6 +167,13 @@ class AuthController extends Controller
                     'error' => $validateData->errors(),
                 ], 200);
             };
+
+            if ($requestedData['confirm_password'] !== $requestedData['new_password']) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Password baru dan confirm password tidak sama',
+                ], 200);
+            }
 
             if ($requestedData['old_password'] === $requestedData['new_password']) {
                 return response()->json([
@@ -183,6 +192,59 @@ class AuthController extends Controller
             User::where('id', $request->user()->id)->update([
                 'password' => Hash::make($requestedData['new_password'])
             ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Password berhasil diperbaharui',
+            ], 200);
+        } catch (\Throwable $err) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => $err->getMessage()
+                ],
+                500
+            );
+        }
+    }
+
+    public static function forgetPassword(Request $request)
+    {
+        try {
+            $requestedData = $request->only([
+                'email',
+                'phone_number',
+                'new_password',
+            ]);
+
+            $validateData = Validator::make(
+                $requestedData,
+                [
+                    'email' => 'required',
+                    'phone_number' => 'required',
+                    'new_password' => 'required'
+                ]
+            );
+
+            if ($validateData->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email/nomor telepon/ password tidak boleh kosong',
+                    'error' => $validateData->errors(),
+                ], 200);
+            };
+
+            $user = User::where('email', $requestedData['email'], 'AND')->where('phone_number', $requestedData['phone_number'])->first();
+
+            if (!$user) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Tidak ada email dan nomor telepon yang sesuai',
+                ], 200);
+            }
+
+            $user->password = Hash::make($requestedData['new_password']);
+            $user->save();
 
             return response()->json([
                 'status' => true,
--- routes/api.php
+++ routes/api.php
@@ -4,20 +4,3 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
-
-Route::post('v1/register', function (Request $request) {
-    return AuthController::register($request);
-});
-Route::post('v1/login', function (Request $request) {
-    return AuthController::login($request);
-});
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('v1/logout', function (Request $request) {
-        return AuthController::logout($request);
-    });
-
-    Route::patch('v1/changepassword', function (Request $request) {
-        return AuthController::changePassword($request);
-    });
-});

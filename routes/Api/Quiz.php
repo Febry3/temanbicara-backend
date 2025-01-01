7c6c82ca4c2a3a8376af75afb3dd7131d5259f6d
hafiiddd <hafid.akhyar0411@gmail.com>
2025-01-01 15:45:37 +0700

    add store Answer

--- routes/Api/Quiz.php
+++ routes/Api/Quiz.php
@@ -5,9 +5,20 @@ use App\Http\Controllers\Quiz\QuizController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('Quiz', function (Request $request) {
-    return QuizController::getAllQuestion();
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('Result', function (Request $request) {
+        return QuizController::getAllAnswer($request);
+    });
+    Route::get('Quiz', function (Request $request) {
+        return QuizController::getAllQuestion();
+    });
+
+
+    Route::get('getMaxPoint', function (Request $request) {
+        return QuizController::getMaxPoints();
+    });
 });
-Route::get('Result', function (Request $request) {
-    return QuizController::getAllAnswer();
+Route::post('storeAnswer', function (Request $request) {
+    return QuizController::storeAnswer($request);
 });

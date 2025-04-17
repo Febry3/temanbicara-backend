7c2f5dc4c648492183a1df0d4f2fef781bdc11bf
hafiiddd <hafid.akhyar0411@gmail.com>
2025-04-18 00:38:36 +0700

    setting expire_date to 20 mins

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -45,7 +45,7 @@ class PaymentController extends Controller
             return [
                 'amount' => $request->amount,
                 // 'expired_date' => date('Y-m-d H:i:s', strtotime($midTransResponse['transaction_time'] . '+24 hours')),
-                'expired_date' => now()->addMinutes(1),
+                'expired_date' => now()->addMinutes(20),
                 'bank' => $request->bank,
                 'va_number' => $midTransResponse['va_numbers'][0]['va_number'],
                 'payment_method' => 'Bank Transfer',

1bdbd6e444ca27b1b5c543bbd3e421a0a2100ef8
Fehehehe <febryt22@gmail.com>
2025-05-10 22:11:32 +0700

    feat: add getLastSevenDaysTracking

--- app/Http/Controllers/Tracking/TrackingController.php
+++ app/Http/Controllers/Tracking/TrackingController.php
@@ -143,11 +143,76 @@ class TrackingController extends Controller
         try {
             $lastSevenData = Auth::user()->lastSevenDaysTracking;
 
+            if ($lastSevenData->isEmpty()) {
+                return response()->json([
+                    'status' => true,
+                    'message' => 'No tracking data available',
+                    'data' => null
+                ], 200);
+            }
+
+            $sum = [
+                "mood" => 0,
+                "stress_level" => 0,
+                "bed_time" => 0,
+                "screen_time" => 0,
+                "activity" => 0,
+            ];
+
+            foreach ($lastSevenData as $data) {
+                $sum['mood'] += match ($data['mood_level']) {
+                    "Depressed" => 1,
+                    "Sad" => 2,
+                    "Neutral" => 3,
+                    "Happy" => 4,
+                    "Cheerful" => 5,
+                };
+
+                $sum['stress_level'] += $data['stress_level'];
+
+                $sum['bed_time'] += match ($data['bed_time']) {
+                    "> 8 hours" => 9,
+                    "7-8 hours" => 8,
+                    "6 hours" => 6,
+                    "4-5 hours" => 5,
+                    "< 4 hours" => 3,
+                };
+
+                $sum['screen_time'] += match ($data['screen_time']) {
+                    "< 1 hours" => 1,
+                    "1-3 hours" => 3,
+                    "3-5 hours" => 5,
+                    "5-8 hours" => 8,
+                    "> 8 hours" => 9,
+                };
+
+                $sum['activity'] += match ($data['activity']) {
+                    "< 2 steps" => 1,
+                    "2k-5k steps" => 5,
+                    "5k-7.5k steps" => 7.5,
+                    "7.5k-10k steps" => 10,
+                    "> 10k steps" => 11,
+                };
+            }
+
             return response()->json(
                 [
                     'status' => true,
                     'message' => 'Data berhasil diambil',
-                    'data' => $lastSevenData
+                    'data' => [
+                        'tracking_data' => $lastSevenData,
+                        'average_mood' => match ((int)round($sum['mood'] / count($lastSevenData))) {
+                            1 => 'Depressed',
+                            2 => 'Sad',
+                            3 => 'Neutral',
+                            4 => 'Happy',
+                            5 => 'Cheerful'
+                        },
+                        'average_stress_level' => round($sum['stress_level'] / count($lastSevenData)),
+                        'average_bed_time' => round($sum['bed_time'] / count($lastSevenData)) . " hours",
+                        'average_screen_time' => round($sum['screen_time'] / count($lastSevenData)) . " hours",
+                        'average_activity' => round($sum['activity'] / count($lastSevenData)) . "k steps",
+                    ]
                 ],
                 200
             );
